b876af8ba8d6d28878f0c751ac3dfdcb3610efee
Oscar-c7 <0841774@my.scccd.edu>
2025-11-24 13:38:28 -0800

    Soccer stats tracker: goals, games played, assists

--- main.cpp
+++ main.cpp
@@ -1,13 +1,67 @@
-// CSCI-40 Final Project
-// Author: [Your Name Here]
+// CSCI-40 course project 
+// Name: Oscar Castellanos 
 
-/* 
- * Description:
- *
-*/
+// This program keeps track of statistics in soccer
+//Which will allow users to input information about players in a team
+//Like their names, goals scored, assists, and games played 
+//As well as being able to add and remove player information all through a menu interface
 
-#include <iostream>
+
+#include <iostream> 
+#include <vector>
+#include <fstream> 
+#include <string>
+using namespace std; 
+
+
+struct player {
+    string name; 
+    int goals; 
+    int assists; 
+    int gamesPlayed;
+
+};
+
+vector<player> team; 
+
+
+void addPlayer(); 
+void viewPlayers();
+void updatePlayer();
+void removePlayer();
+void saveToFile();
+void loadFromFile();
+void menu();
 
 int main() {
+    loadFromFile();
+    menu();
     return 0;
 }
+
+void addPlayer() {
+    player p; 
+    cout << "\nEnter player name: ";
+    cin.ignore();
+    getline(cin, p.name);
+
+    cout << "Goals scored: ";
+    cin >> p.goals; 
+
+    cout << "assists: ";
+    cin >> p.assists; 
+
+    cout << "games played: ";
+    cin >> p.gamesPlayed; 
+
+    team.push_back(p);
+    cout << "\nPlayer added successfully!\n";
+
+}
+
+void viewPlayers() {
+    if (team.empty()) {
+        cout << "\nNo players available.\n";
+        return; 
+    }
+} 
